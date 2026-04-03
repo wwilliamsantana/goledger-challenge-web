f692ac2c4008abc067cc9645e8aa67da84485553
William <euwilliamsantana@gmail.com>
2026-04-03 10:00:59 -0300

    feat: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 
 export default function Home() {
   return (
-    <div>Hello Word</div>
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-5xl font-bold text-blue-600 tracking-wide">
+        Hello, Will
+      </h1>
+    </div>
   );
 }
